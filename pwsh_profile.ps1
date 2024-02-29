6a93340f21a24a8a7df700fd06bb9bd176659f0d
Saúl Márquez Martín <saul.marquez.martin@gmail.com>
2024-02-29 16:53:09 +0100

    add pwsh init script

---
+++ pwsh_profile.ps1
@@ -0,0 +1,189 @@
+oh-my-posh init pwsh --config "C:\\dev\\configs\\prompt\\cloud-context.omp.json" | Invoke-Expression
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
+[console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+Set-Alias -Name wget -Value wget2
+
+# $env:POSH_GIT_ENABLED = $true
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    $completion_file = New-TemporaryFile
+    $env:ARGCOMPLETE_USE_TEMPFILES = 1
+    $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+    $env:COMP_LINE = $wordToComplete
+    $env:COMP_POINT = $cursorPosition
+    $env:_ARGCOMPLETE = 1
+    $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+    $env:_ARGCOMPLETE_IFS = "`n"
+    $env:_ARGCOMPLETE_SHELL = 'powershell'
+    az 2>&1 | Out-Null
+    Get-Content $completion_file | Sort-Object | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+    Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+}
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
+function Get-FileFromWeb {
+    param (
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [string]$URL,
+  
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [string]$File 
+    )
+    Begin {
+        function Show-Progress {
+            param (
+                # Enter total value
+                [Parameter(Mandatory)]
+                [Single]$TotalValue,
+        
+                # Enter current value
+                [Parameter(Mandatory)]
+                [Single]$CurrentValue,
+        
+                # Enter custom progresstext
+                [Parameter(Mandatory)]
+                [string]$ProgressText,
+        
+                # Enter value suffix
+                [Parameter()]
+                [string]$ValueSuffix,
+        
+                # Enter bar lengh suffix
+                [Parameter()]
+                [int]$BarSize = 40,
+
+                # show complete bar
+                [Parameter()]
+                [switch]$Complete
+            )
+            
+            # calc %
+            $percent = $CurrentValue / $TotalValue
+            $percentComplete = $percent * 100
+            if ($ValueSuffix) {
+                $ValueSuffix = " $ValueSuffix" # add space in front
+            }
+            if ($psISE) {
+                Write-Progress "$ProgressText $CurrentValue$ValueSuffix of $TotalValue$ValueSuffix" -id 0 -percentComplete $percentComplete            
+            }
+            else {
+                # build progressbar with string function
+                $curBarSize = $BarSize * $percent
+                $progbar = ""
+                $progbar = $progbar.PadRight($curBarSize,[char]9608)
+                $progbar = $progbar.PadRight($BarSize,[char]9617)
+        
+                if (!$Complete.IsPresent) {
+                    Write-Host -NoNewLine "`r$ProgressText $progbar [ $($CurrentValue.ToString("#.###").PadLeft($TotalValue.ToString("#.###").Length))$ValueSuffix / $($TotalValue.ToString("#.###"))$ValueSuffix ] $($percentComplete.ToString("##0.00").PadLeft(6)) % complete"
+                }
+                else {
+                    Write-Host -NoNewLine "`r$ProgressText $progbar [ $($TotalValue.ToString("#.###").PadLeft($TotalValue.ToString("#.###").Length))$ValueSuffix / $($TotalValue.ToString("#.###"))$ValueSuffix ] $($percentComplete.ToString("##0.00").PadLeft(6)) % complete"                    
+                }                
+            }   
+        }
+    }
+    Process {
+        try {
+            $storeEAP = $ErrorActionPreference
+            $ErrorActionPreference = 'Stop'
+        
+            # invoke request
+            $request = [System.Net.HttpWebRequest]::Create($URL)
+            $response = $request.GetResponse()
+  
+            if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) {
+                throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'."
+            }
+  
+            if($File -match '^\.\\') {
+                $File = Join-Path (Get-Location -PSProvider "FileSystem") ($File -Split '^\.')[1]
+            }
+            
+            if($File -and !(Split-Path $File)) {
+                $File = Join-Path (Get-Location -PSProvider "FileSystem") $File
+            }
+
+            if ($File) {
+                $fileDirectory = $([System.IO.Path]::GetDirectoryName($File))
+                if (!(Test-Path($fileDirectory))) {
+                    [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null
+                }
+            }
+
+            [long]$fullSize = $response.ContentLength
+            $fullSizeMB = $fullSize / 1024 / 1024
+  
+            # define buffer
+            [byte[]]$buffer = new-object byte[] 1048576
+            [long]$total = [long]$count = 0
+  
+            # create reader / writer
+            $reader = $response.GetResponseStream()
+            $writer = new-object System.IO.FileStream $File, "Create"
+  
+            # start download
+            $finalBarCount = 0 #show final bar only one time
+            do {
+          
+                $count = $reader.Read($buffer, 0, $buffer.Length)
+          
+                $writer.Write($buffer, 0, $count)
+              
+                $total += $count
+                $totalMB = $total / 1024 / 1024
+          
+                if ($fullSize -gt 0) {
+                    Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText "Downloading $($File.Name)" -ValueSuffix "MB"
+                }
+
+                if ($total -eq $fullSize -and $count -eq 0 -and $finalBarCount -eq 0) {
+                    Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText "Downloading $($File.Name)" -ValueSuffix "MB" -Complete
+                    $finalBarCount++
+                    #Write-Host "$finalBarCount"
+                }
+
+            } while ($count -gt 0)
+        }
+  
+        catch {
+        
+            $ExeptionMsg = $_.Exception.Message
+            Write-Host "Download breaks with error : $ExeptionMsg"
+        }
+  
+        finally {
+            # cleanup
+            if ($reader) { $reader.Close() }
+            if ($writer) { $writer.Flush(); $writer.Close() }
+        
+            $ErrorActionPreference = $storeEAP
+            [GC]::Collect()
+        }    
+    }
+}
