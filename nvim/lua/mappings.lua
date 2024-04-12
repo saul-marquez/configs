60683e27386f2f81729dfd4173d298ac7efc4d10
Saul Marquez <saul.marquez.martin@gmail.com>
2024-04-12 11:36:32 +0200

    <ESC> -> :noh

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -12,6 +12,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Better :noh
+vim.keymap.set('n', '<ESC>', vim.cmd.nohlsearch, { desc = 'Clear highlights' })
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
