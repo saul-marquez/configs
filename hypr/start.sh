ecb1263346784da5ff2fb1604895de7faa7d3b21
Saul Marquez <saul.marquez.martin@gmail.com>
2024-10-06 12:00:55 +0200

    refactor

--- hypr/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-swww-daemon &
-swww img ~/Downloads/wenniel-lun-pe_R74hldW4-unsplash.jpg &
-nm-applet --indicator &
-waybar &
-dunst
--- i3/startup.sh
+++
@@ -1,3 +0,0 @@
-i3-msg "workspace ; exec --no-startup-id alacritty";
-sleep 2;
-i3-msg "exec --no-startup-id teams-for-linux";
